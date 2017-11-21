c9bf52a2efae26a75de0903fd2d66eece965c2c6
Yael <ygoldin@hotmail.com>
2017-11-20 19:29:03 -0800

    created lazy bot (picks first open spot). Added more logic to playing vs. bot

--- src/gameplay/PlayAgainstBot.java
+++ src/gameplay/PlayAgainstBot.java
@@ -1,14 +1,27 @@
 package gameplay;
 
 import java.util.Scanner;
+import setup.Board;
+import setup.Minimax;
 
 public class PlayAgainstBot {
 	private static final Scanner INPUT = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		do {
-			
+			playOneGame();
 		} while(playAgain());
+	}
+	
+	private static void playOneGame() {
+		System.out.println("Welcome to Tic-Tac-Toe!");
+		int playerGoingFirst;
+		do {
+			System.out.println("Do you want to go first (1) or second (2)? ");
+			playerGoingFirst = INPUT.nextInt();
+			INPUT.nextLine();
+		} while(playerGoingFirst != 1 && playerGoingFirst != 2);
+		Board gameBoard = new Board(playerGoingFirst);
 	}
 	
 	private static boolean playAgain() {
--- src/setup/GridPosition.java
+++ src/setup/GridPosition.java
@@ -1,24 +1,10 @@
 package setup;
 
 public class GridPosition {
-	int row, col;
-	char value;
+	public int row, col;
 	
 	public GridPosition(int row, int col) {
-		this(row, col, ' ');
-	}
-	
-	private GridPosition(int row, int col, char value) {
 		this.row = row;
 		this.col = col;
-		this.value = value;
-	}
-	
-	public boolean isEmpty() {
-		return value == ' ';
-	}
-	
-	public GridPosition copy() {
-		return new GridPosition(row, col, value);
 	}
 }
